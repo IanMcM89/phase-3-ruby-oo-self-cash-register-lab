a242dd6af748c6390bf49cc96a93383ee532f48f
Ian <ianmcm.89@gmail.com>
2022-01-28 20:15:34 -0500

    commit tree 28f39dff90703077511de409d9811364082bd484

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,26 @@
+class CashRegister
+    attr_accessor :total, :discount, :items
 
+    def initialize(discount)
+        @total = 0
+        @discount = discount
+        @items = []
+    end
+
+    def total
+        @total
+    end
+
+    def add_item(title, price, quantity)
+        self.total+= price
+        @items << title
+    end
+
+    def apply_discount
+
+    end
+
+    def items()
+        @items
+    end
+end
